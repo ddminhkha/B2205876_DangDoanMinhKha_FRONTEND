c48c6478c8ffcb4829a744217fb1ef3b255337d7
Đặng Đoàn Minh Kha <khab2205876@student.ctu.edu.vn>
2025-10-31 19:23:22 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true,
-    }
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true,
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
